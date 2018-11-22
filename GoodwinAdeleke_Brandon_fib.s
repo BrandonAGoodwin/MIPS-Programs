8b93ed5464145e4abcd46b762e6a32542bec72d3
BrandonAGoodwin <35398295+BrandonAGoodwin@users.noreply.github.com>
2018-11-22 17:34:26 +0000

    More progress on fib

--- GoodwinAdeleke_Brandon_fib.s
+++ GoodwinAdeleke_Brandon_fib.s
@@ -6,7 +6,6 @@ input:  .space 1024
     .text
 main:
 
-start:
     la $a0, pr
     li $v0, 4
     syscall                 # Print out input requesst to command line
@@ -22,6 +21,22 @@ start:
     move $a0, $s0
     jal CHECK               # Check the user input represents a string otherwise ask for input again
     
+    # Allocate heap space for array of already processed results
+    
+    # Loop fib from 0 to user input
+loop:
+    # All temporaries need to be changed
+    beqz $t0, end
+    
+    
+    jal FIB
+    
+    move $a0, $v0
+    move $t0, $v0
+    li $v0, 1
+    syscall                 # Print out output of FIB
+    j loop
+end:
     li $v0, 10              
     syscall                 # End the program
 
@@ -29,20 +44,50 @@ start:
 # $a0: String to be checked
 # $v0: Integer (if string represents an integer)
 CHECK:
-    addi $sp, $sp, 0
-    sw $ra, ($sp)
+    addi $sp, $sp, -12
+    sw $ra, 8($sp)
+    sw $s0, 4($sp)
+    sw $s1, 0($sp)
     
     move $s0, $a0           # $s0: Stores the address of the string
     
+    li $s1, 0
+    li $t9, 48              # $t9: Holds the ASCII value offset for digits 0-9
     move $t0, $a0
     li $t1, 0               # $t1: Stores the pointer to the current character
     # ASCII offset = 48
 check_loop:
-
+    lb $t1, 0($s0)
+    # Might be good practice to set the stack pointer right before ending the program
+    beqz $t1, end_of_string # Branch of the end of string is reached
     
-    if
+    sub $t1, $t1, $t9       # Sub the ASCII value offset to get the digit
+    
+    bltz $t1, invalid_input
+    addi $t2, $t1, -9
+    
+    bgtz $t2, invalid_input
+    
+    multi $s1, 10
+    add $s1, $s1, $t1
+    j check_loop
+    
+    addi $s0, $s0, 1
+invalid_input:
+    lw $ra, 8($sp)
+    lw $s0, 4($sp)
+    lw $s1, 0($sp)
+    addi $sp, $sp, 12
+    la $a0, wi
+    li $v0, 4
+    syscall                 # Print invalid input message to the console
+    j main                  # Jump back to the beginning of the code
+end_of_string:
 
+# $a0: The position of the value in the fibonacci sequence we want to return
+# $a1: The array that stores any pre-calculated values / to store newly calculated values
 FIB:
     addi $sp, $sp, 0
     sw $ra, ($sp)
+
     
